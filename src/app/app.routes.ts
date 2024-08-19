99bae74d987665c61f7b353595e37d9d5624ed67
Dennis.B <Baust.Dennis@gmail.com>
2024-08-19 17:53:19 +0200

    login first steps

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,37 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './modules/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: LoginComponent
+    },
+    {
+        path: 'summary',
+        loadComponent: () => import('./modules/summary/summary.component').then(m => m.SummaryComponent)
+    },
+    {
+        path: 'add-task',
+        loadComponent: () => import('./modules/add-task/add-task.component').then(m => m.AddTaskComponent)
+    },
+    {
+        path: 'board',
+        loadComponent: () => import('./modules/board/board.component').then(m => m.BoardComponent)
+    },
+    {
+        path: 'contacts',
+        loadComponent: () => import('./modules/contacts/contacts.component').then(m => m.ContactsComponent)
+    },
+    {
+        path: 'legal-notice',
+        loadComponent: () => import('./modules/legal-notice/legal-notice.component').then(m => m.LegalNoticeComponent)
+    },
+    {
+        path: 'privacy-policy',
+        loadComponent: () => import('./modules/privacy-policy/privacy-policy.component').then(m => m.PrivacyPolicyComponent)
+    },
+    {
+        path: 'help',
+        loadComponent: () => import('./modules/help/help.component').then(m => m.HelpComponent)
+    }
+];
