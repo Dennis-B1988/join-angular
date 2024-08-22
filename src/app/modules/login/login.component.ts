3e497b790df3defb20d54d0767a9c7a00a8d88c4
Dennis.B <Baust.Dennis@gmail.com>
2024-08-22 02:28:02 +0200

    login and signup design

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -12,4 +12,21 @@ export class LoginComponent {
 
   showLogin: boolean = true;
   checked: boolean = false;
+  backToLogin: string = 'images/back.png';
+  showSuccess: boolean = false;
+
+
+  constructor() { }
+
+
+  signUpSuccess() {
+    this.showSuccess = true;
+    let interval = setInterval(() => {
+      if (this.showSuccess) {
+        this.showSuccess = false;
+        clearInterval(interval)
+      }
+    }, 1000)
+
+  }
 }
