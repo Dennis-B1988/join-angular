9a59fabeea516169ebcc378293ef56a8801e644d
Dennis.B <Baust.Dennis@gmail.com>
2024-08-26 23:53:29 +0200

    changed firebase data visibility

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,13 +5,14 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getDatabase, provideDatabase } from '@angular/fire/database';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from '../environment/environment';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideFirebaseApp(() => initializeApp({ "projectId": "join-angular-d79e8", "appId": "1:898243239657:web:781934f1220f85f4349a78", "storageBucket": "join-angular-d79e8.appspot.com", "apiKey": "AIzaSyAMaqgIOk3PVW2reJugIKLcdsDyiRGo6p0", "authDomain": "join-angular-d79e8.firebaseapp.com", "messagingSenderId": "898243239657" })),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
     provideDatabase(() => getDatabase())
---
+++ src/environment/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+        projectId: "join-angular-d79e8",
+        appId: "1:898243239657:web:781934f1220f85f4349a78",
+        storageBucket: "join-angular-d79e8.appspot.com",
+        apiKey: "AIzaSyAMaqgIOk3PVW2reJugIKLcdsDyiRGo6p0",
+        authDomain: "join-angular-d79e8.firebaseapp.com",
+        messagingSenderId: "898243239657"
+    }
+};
